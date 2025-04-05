a236ebb2b0cb9b5dcd2ce80a724bc8e53337bdab
asa129 <asa.jad1290@gmail.com>
2025-04-06 07:47:10 +0900

    レイアウト変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Box, Divider, Heading } from "@chakra-ui/react";
+
 function App() {
   return (
     <>
-      <h1 data-testid="title">Hello World</h1>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        h="100vh"
+        w="100vw"
+        border="1px solid red"
+      >
+        <Box textAlign="center">
+          <Heading as="h1" data-testid="title">
+            新・学習記録アプリ
+          </Heading>
+          <Divider m={4} />
+        </Box>
+      </Box>
     </>
   );
 }
