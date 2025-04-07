59970fb1e2555d311094038acc4094cd50b75bed
asa129 <asa.jad1290@gmail.com>
2025-04-07 22:02:00 +0900

    モック追加

--- src/tests/AppComponent.spec.tsx
+++ src/tests/AppComponent.spec.tsx
@@ -1,8 +1,17 @@
 import App from "../App";
 import { render, screen } from "@testing-library/react";
+import { GetAllStudyRecords } from "../lib/study-record";
+
+// 非同期関数をモック
+jest.mock("../lib/study-record", () => ({
+  GetAllStudyRecords: jest.fn(),
+}));
 
 describe("title", () => {
   it("should render title", async () => {
+    // モックの戻り値を設定
+    (GetAllStudyRecords as jest.Mock).mockResolvedValue([]);
+
     render(<App />);
     expect((await screen.findByTestId("title")).textContent).toBe(
       "新・学習記録アプリ"
