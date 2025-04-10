dc122fe4d40ce5858e81cd6a26acdbcb0f8e7ddd
asa129 <asa.jad1290@gmail.com>
2025-04-10 22:46:17 +0900

    ライブラリ追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import {
   Box,
+  Button,
   Divider,
+  FormControl,
+  FormLabel,
   Heading,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
   Spinner,
   Table,
   TableContainer,
@@ -10,14 +26,19 @@ import {
   Th,
   Thead,
   Tr,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { GetAllStudyRecords } from "./lib/study-record";
 import { Record } from "./domain/record";
+import { BsPencil } from "react-icons/bs";
+import React from "react";
 
 function App() {
   const [studyRecords, setStudyRecords] = useState<Record[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = React.useRef(null);
   useEffect(() => {
     const getAllStudyRecords = async () => {
       const records = await GetAllStudyRecords();
@@ -26,6 +47,7 @@ function App() {
     };
     getAllStudyRecords();
   }, []);
+
   if (isLoading) {
     return (
       <Box
@@ -59,32 +81,71 @@ function App() {
           <Heading as="h1" data-testid="title">
             新・学習記録アプリ
           </Heading>
-          <Divider m={4} />
+          <Divider m={4} w="20vw" />
         </Box>
-        <TableContainer>
-          <Table variant="striped" colorScheme="teal">
-            <Thead>
-              <Tr>
-                <Th>title</Th>
-                <Th>time</Th>
-                <Th></Th>
-                <Th></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {studyRecords.map((studyRecord) => {
-                return (
-                  <Tr key={studyRecord.id}>
-                    <Td>{studyRecord.title}</Td>
-                    <Td isNumeric>{studyRecord.time}</Td>
-                    <Td></Td>
-                    <Td></Td>
-                  </Tr>
-                );
-              })}
-            </Tbody>
-          </Table>
-        </TableContainer>
+        <Box display="flex" justifyContent="flex-end" w="40%" mb={4}>
+          <Button
+            leftIcon={<BsPencil />}
+            colorScheme="teal"
+            variant="solid"
+            onClick={onOpen}
+          >
+            登録
+          </Button>
+          <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Modal Title</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <FormControl isRequired>
+                  <FormLabel>学習内容</FormLabel>
+                  <Input ref={initialRef} placeholder="学習内容" />
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>学習時間</FormLabel>
+                  <NumberInput defaultValue={0} max={50} min={0}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </FormControl>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button colorScheme="teal">登録</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </Box>
+        <Box w="40%">
+          <TableContainer>
+            <Table variant="striped" colorScheme="teal">
+              <Thead>
+                <Tr>
+                  <Th>title</Th>
+                  <Th>time</Th>
+                  <Th>編集</Th>
+                  <Th>削除</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {studyRecords.map((studyRecord) => {
+                  return (
+                    <Tr key={studyRecord.id}>
+                      <Td>{studyRecord.title}</Td>
+                      <Td isNumeric>{studyRecord.time}</Td>
+                      <Td>編集</Td>
+                      <Td>削除</Td>
+                    </Tr>
+                  );
+                })}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
       </Box>
     </>
   );
